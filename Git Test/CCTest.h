482c8e02ecafa317401ee1312fda6f11f6cc7617
Hisham Samir <hishamsamir@Hishams-MacBook-Pro.local>
2014-06-04 12:01:30 +0200

    added Test Class

---
+++ Git Test/CCTest.h
@@ -0,0 +1,13 @@
+//
+//  CCTest.h
+//  Git Test
+//
+//  Created by Hisham Samir on 6/4/14.
+//  Copyright (c) 2014 Hisham Samir. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface CCTest : NSObject
+
+@end
---
+++ Git Test/CCTest.m
@@ -0,0 +1,13 @@
+//
+//  CCTest.m
+//  Git Test
+//
+//  Created by Hisham Samir on 6/4/14.
+//  Copyright (c) 2014 Hisham Samir. All rights reserved.
+//
+
+#import "CCTest.h"
+
+@implementation CCTest
+
+@end
